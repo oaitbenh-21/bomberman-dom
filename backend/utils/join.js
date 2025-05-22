cfcd614dfbd66f4068f004f5f26b9e2c1eef2398
obenali <oussama.benali@ump.ac.ma>
2025-05-22 20:34:58 +0100

    comment and edit one line in the join.js file

--- backend/utils/join.js
+++ backend/utils/join.js
@@ -4,21 +4,34 @@ const Connections = [];
 const colors = ["red", "blue", "yellow", "green"];
 
 export function JoinPlayer(ws) {
-      Connections.push(ws)
+      Connections.push(ws) // add ws to the connection pool
+
       let currentRoom;
-      let nowPlayer;
+      let currentPlayer;
+      // get last room index
       const lastRoom = Rooms[Rooms.length - 1];
+
+      // check if possible to add to the last room
       if (lastRoom && lastRoom.Players.length < 4 && lastRoom.Waiting && !lastRoom.Over) {
             currentRoom = lastRoom;
-            nowPlayer++;
-      } else {
+
+            //currentPlayer++;  // i changed this because it could be undefined and it will cause something unexpected
+            currentPlayer = lastRoom.Players.length;
+
+      } else { // create new room 
             currentRoom = new Room();
             Rooms.push(currentRoom);
-            nowPlayer = 0;
+            currentPlayer = 0;
       }
+
       if (currentRoom.Over) return;
+
+      // create a new player and set it's color
       const player = new Player(currentRoom, ws);
-      player.color = colors[nowPlayer]
+      player.color = colors[currentPlayer]
+
+      // add the new player to the current room
       currentRoom.addPlayer(player, ws);
+
       return [player, currentRoom]
 }
