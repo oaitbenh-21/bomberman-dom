972d47d3bd708e90660281a015ebe7a04b9084e5
obenali <oussama.benali@ump.ac.ma>
2025-05-23 11:52:23 +0100

    mvoe the event manager to the mini-framework

--- frontend/app.js
+++ frontend/app.js
@@ -1,62 +1,8 @@
-import { createElement, render } from "https://cdn.jsdelivr.net/npm/mini-framework-z01@1.0.7/dist/mini-framework-z01.min.js";
+import { createElement, render, EventManager } from "https://cdn.jsdelivr.net/npm/mini-framework-z01@1.0.7/dist/mini-framework-z01.min.js";
 import renderBoard from "./components/board.js";
 import renderHeader from "./components/header.js";
 import renderChat from "./components/chat.js";
 import Socket from "./src/socket.js";
-
-class EventManager {
-    constructor() {
-        this.customEvents = {};
-        this.nativeListeners = new Map(); // Tracks native event handlers
-    }
-
-    isNativeEvent(eventName) {
-        const nativeEvents = ['keydown', 'keyup', 'click', 'scroll', 'mousedown', 'mouseup'];
-        return nativeEvents.includes(eventName);
-    }
-
-    on(eventName, callback, element = window) {
-        if (!this.customEvents[eventName]) { // the event isn't registered
-            this.customEvents[eventName] = []; // register the event
-
-            // Setup native event listener if applicable
-            if (this.isNativeEvent(eventName)) {
-                const handler = (e) => this.emit(eventName, e);
-                element.addEventListener(eventName, handler);
-                this.nativeListeners.set(eventName, { element, handler });
-            }
-        }
-        // event registered
-        this.customEvents[eventName].push(callback); // overite the event 
-    }
-
-    off(eventName, callback) {
-        if (!this.customEvents[eventName]) return;
-
-        // Remove the callback
-        this.customEvents[eventName] = this.customEvents[eventName].filter(
-            cb => cb !== callback
-        );
-
-        // If no more callbacks, remove native listener
-        if (this.customEvents[eventName].length === 0) {
-            const nativeListener = this.nativeListeners.get(eventName);
-            if (nativeListener) {
-                nativeListener.element.removeEventListener(eventName, nativeListener.handler);
-                this.nativeListeners.delete(eventName);
-            }
-            delete this.customEvents[eventName];
-        }
-    }
-
-    emit(eventName, data) {
-        const listeners = this.customEvents[eventName];
-        if (listeners) {
-            listeners.forEach(cb => cb(data));
-        }
-    }
-}
-
 
 class App {
     constructor() {
