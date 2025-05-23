5c2c0cef09bc908b46c2fa1ee806ee117960e158
obenali <oussama.benali@ump.ac.ma>
2025-05-23 23:40:13 +0100

    fix the error of rerendering there is no problem when chatting

--- frontend/components/chat.js
+++ frontend/components/chat.js
@@ -1,50 +1,80 @@
-import { createElement } from "https://cdn.jsdelivr.net/npm/mini-framework-z01@1.0.7/dist/mini-framework-z01.min.js";
+import {
+    createElement,
+    createStore,
+} from "https://cdn.jsdelivr.net/npm/mini-framework-z01@1.0.7/dist/mini-framework-z01.min.js";
+
+const message = createStore("");
 
 const renderChat = (messages = [], ws, isChating) => {
-      const sendMessage = (message) => {
-            const data = {
-                  type: "chat-client",
-                  message: message
-            };
-            ws.send(JSON.stringify(data));
-      }
-      return createElement('div', { class: 'chat' },
+
+    const sendMessage = (message) => {
+        const data = {
+            type: "chat-client",
+            message: message,
+        };
+        ws.send(JSON.stringify(data));
+    };
+
+    return createElement("div", { class: "chat" }, [
+        createElement(
+            "div",
+            { class: "chat-messages" },
+            messages.map((message) =>
+                createElement("div", { class: "chat-message" }, [
+                    createElement(
+                        "span",
+                        { class: "chat-user" },
+                        `${message.username}: `
+                    ),
+                    createElement(
+                        "span",
+                        { class: "chat-text" },
+                        message.message
+                    ),
+                ])
+            )
+        ),
+        createElement(
+            "form",
+            {
+                class: "chat-input",
+                onsubmit: (e) => {
+                    e.preventDefault();
+                    //   const input = e.target.elements.chat;
+                    const msg = message.getState().trim();
+                    if (msg) {
+                        sendMessage(msg);
+                        message.setState("");
+                    }
+                },
+            },
             [
-                  createElement('div', { class: 'chat-messages' },
-                        messages.map((message) =>
-                              createElement('div', { class: 'chat-message' }, [
-                                    createElement('span', { class: 'chat-user' }, `${message.username}: `),
-                                    createElement('span', { class: 'chat-text' }, message.message)
-                              ])
-                        )
-                  ),
-                  createElement('form', {
-                        class: 'chat-input', onsubmit: (e) => {
-                              e.preventDefault();
-                              const input = e.target.elements.chat;
-                              const msg = input.value.trim();
-                              if (msg) {
-                                    sendMessage(msg);
-                                    input.value = '';
-                              }
-                        }
-                  }, [
-                        createElement('input', {
-                              onclick: () => {
-                                    console.log('input clicked, chating:', isChating);
-                                    isChating.setState(true)
-                                    console.log('did it change:', isChating);
-                              },
-                              type: 'text',
-                              name: 'chat',
-                              placeholder: 'Type your message...',
-                              autocomplete: 'off'
-                        }),
-                        createElement('button', { type: 'submit' }, createElement('img', { src: '../assets/img/send.png', alt: "send" }))
-                  ]
-                  )
+                createElement("input", {
+                    onclick: () => {
+                        console.log("input clicked, chating:", isChating);
+                        isChating.setState(true);
+                        console.log("did it change:", isChating);
+                    },
+                    onchange: (e) => {
+                        message.setState(e.target.value);
+                    },
+                    value: message.getState(),
+                    type: "text",
+                    name: "chat",
+                    placeholder: "Type your message...",
+                    autocomplete: "off",
+                }),
+                createElement(
+                    "button",
+                    { type: "submit" },
+                    createElement("img", {
+                        src: "../assets/img/send.png",
+                        alt: "send",
+                    })
+                ),
             ]
-      );
+        ),
+    ]);
 };
 
 export default renderChat;
