01849f558d94ebffec069aa1d6d7f6521d6b0bff
oaitbenh <0xmrrandom@gmail.com>
2025-06-02 22:17:55 +0100

    headers are working

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,4 +1,4 @@
-import { createElement, createSignal } from "../../mini-framework/src/mini-framework-z01.js"
+import { appendTo, createElement, createSignal } from "../../mini-framework/src/mini-framework-z01.js"
 import { effect } from "../../mini-framework/src/mini-framework-z01.js";
 
 let HeaderSignals = createSignal({ count: 0, time: "00:00", lifes: 0, bombs: 0, });
@@ -16,8 +16,7 @@ const renderHeader = (gameData) => {
           effect(() => {
             const data = HeaderSignals.get();
             el.textContent = `Players: ${data.count}/4`;
-            // el.querySelector('img')?.src = './assets/img/player.png';
-            // el.querySelector('img')?.alt = "player";
+            appendTo(el, createElement('img', { src: './assets/img/player.png', alt: "player" }));
           });
         }
       }, [
@@ -30,13 +29,11 @@ const renderHeader = (gameData) => {
           effect(() => {
             const data = HeaderSignals.get();
             el.textContent = `Time: ${data.time}`;
-            // el.querySelector('img')?.src = './assets/img/timer.png';
-            // el.querySelector('img')?.alt = "timer";
+            appendTo(el, createElement('img', { src: './assets/img/timer.png', alt: "timer" }));
           });
         }
       }, [
         `Time: ${gameData.time}`,
-        createElement('img', { src: './assets/img/timer.png', alt: "timer" })
       ]),
       createElement('div', {
         class: 'item',
@@ -44,13 +41,11 @@ const renderHeader = (gameData) => {
           effect(() => {
             const data = HeaderSignals.get();
             el.textContent = `Lifes: ${data.lifes}`;
-            // el.querySelector('img')?.src = './assets/img/lifes.png';
-            // el.querySelector('img')?.alt = "heart";
+            appendTo(el, createElement('img', { src: './assets/img/lifes.png', alt: "lifes" }));
           });
         }
       }, [
         `Lifes: ${gameData.lifes}`,
-        createElement('img', { src: './assets/img/lifes.png', alt: "heart" })
       ]),
       createElement('div', {
         class: 'item',
@@ -58,13 +53,11 @@ const renderHeader = (gameData) => {
           effect(() => {
             const data = HeaderSignals.get();
             el.textContent = `Bombs: ${data.bombs}`;
-            // el.querySelector('img')?.src = './assets/img/bombs.png';
-            // el.querySelector('img')?.alt = "bomb";
+            appendTo(el, createElement('img', { src: './assets/img/bombs.png', alt: "bomb" }));
           });
         }
       }, [
         `Bombs: ${gameData.bombs}`,
-        createElement('img', { src: './assets/img/bombs.png', alt: "bomb" })
       ])
     ]
   );
