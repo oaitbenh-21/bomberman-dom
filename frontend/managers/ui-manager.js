0e29c73bae968a1e7e11b6c4e863beffa5a3888e
obenali <oussama.benali@ump.ac.ma>
2025-06-02 21:59:18 +0100

    lis sami

--- frontend/managers/ui-manager.js
+++ frontend/managers/ui-manager.js
@@ -1,33 +1,35 @@
 import {
     render,
     createElement,
+    effect,
 } from "../../mini-framework/src/mini-framework-z01.js";
 import renderHeader from "../components/header.js";
 import renderChat from "../components/chat.js";
 import renderBoard from "../components/board.js";
 import renderWelcome from "../components/Welcome.js";
 
-export default function renderUI(container, gameState, socket, isChating) {
-    console.log("Rendering UI with state:", gameState);
+export default function initUI(container, gameState, socket, isChating) {
     const state = gameState.getState();
-    const boxes = state.board.length ? state.board : [];
-    const content = state.player ?
-        createElement("div", { class: "container" }, [
-            renderHeader(state.gameData),
-            createElement("div", { class: "game" }, [
-                renderBoard(
-                    boxes,
-                    state.players,
-                    isChating
-                ),
-                renderChat(socket, isChating),
-            ]),
-        ])
-        :
-        renderWelcome(
-            socket,
-            gameState
-        );
+    // This effect reruns when `player` changes
+    effect(() => {
+        const player = state.player.get();
+       /*  */ console.log('rendering the init ui, the state is :', player)
 
-    render(content, container);
+        // Decide what to render based on whether player is set
+        const content = player
+            ? createElement("div", { class: "container" }, [
+                renderHeader(state.gameData),
+                createElement("div", { class: "game" }, [
+                    renderBoard(
+                        state.board,
+                        state.players,
+                        isChating
+                    ),
+                    renderChat(state.messages, socket, isChating),
+                ]),
+            ])
+            : renderWelcome(socket, gameState); // show name input
+
+        render(content, container); // this replaces the DOM content
+    });
 }
