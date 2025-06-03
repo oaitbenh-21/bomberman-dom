bded174289ea42dedc32699a549efb6c33379337
melfihry <fihrylara@gmail.com>
2025-06-03 19:11:49 +0100

    refactor endGame function to display winner and render to container

--- frontend/components/endGame.js
+++ frontend/components/endGame.js
@@ -1,9 +1,15 @@
-import { createElement } from "../../mini-framework/src/mini-framework-z01.js"; 
-export const endGame = (status) => {
-    return createElement('div', { class: "GameStatus" }, [
-        createElement('div', { class: "StausContainer", style: `display: ${status.number == 0 ? "flex" : "none"};` }, [
-            createElement('div', { class: "title" }, status.title),
-            createElement('div', { class: "message" }, status.message),
-        ]),
-    ]);
+import { createElement, render, } from "../../mini-framework/src/mini-framework-z01.js";
+
+
+function endGame(gameState, container) {
+      const state = gameState.getState();
+      const name = state.name.get();
+      const winner = state.winner;
+      const endGame = createElement("div", { class: "waitingRoom" }, [
+            createElement("h2", {}, [`Game over the winner is ${winner}`])
+      ]);
+      render(endGame, container);
+
 }
+
+export default endGame;
