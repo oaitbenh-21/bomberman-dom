927bfa912a1db19011d14899a2ffd867bc3974b1
oaitbenh-21 <0xmrrandom@gmail.com>
2025-06-03 16:57:55 +0100

    try

--- frontend/components/players.js
+++ frontend/components/players.js
@@ -10,6 +10,7 @@ const playerSignals = {};
  * Initialize players with signals and refs.
  */
 export function setPlayers(players) {
+  console.log(players);
   players.forEach(player => {
     if (!playerSignals[player.id]) {
       playerSignals[player.id] = createSignal({ ...player.pos });
@@ -29,6 +30,35 @@ export function setPlayerPosition(id, newPos) {
   }
 }
 
+
+/**
+ * Render players with reactive position effects.
+ */
+// const Players = () => {
+//   return createElement(
+//     "div",
+//     { class: "Players" },
+//     Object.entries(playerSignals).map(([id, signal]) => {
+//       return createElement("img", {
+//         class: "player",
+//         src: "./assets/img/down-1.png",
+//         style: {
+//           position: "absolute",
+//           transform: `translate(${signal.get().x * 40}px, ${signal.get().y * 40}px)`, // initial render
+//         },
+//         alt: "player",
+//         id,
+//         key: id,
+//         onMount(el) {
+//           effect(() => {
+//             const { x, y } = signal.get();
+//             el.style.transform = `translate(${x}px, ${y}px)`;
+//           });
+//         }
+//       });
+//     })
+//   );
+// };
 /**
  * Render players with reactive position effects.
  */
