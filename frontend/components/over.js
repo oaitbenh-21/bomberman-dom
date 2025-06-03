ade006db1fce598ad5dbc06fd919710eebb45e82
ielharra <ielharra@gmail.com>
2025-06-03 20:02:54 +0100

    more fix to the ui

--- frontend/components/over.js
+++ frontend/components/over.js
@@ -1,11 +1,16 @@
 import { createElement, render } from "../../mini-framework/src/mini-framework-z01.js";
 
 export default function renderPopup(_, container) {
+    // Clear any existing countdown interval
+    if (container._popupInterval) {
+        clearInterval(container._popupInterval);
+    }
+
     let countdown = 10;
 
     function renderCountdown() {
         const popup = createElement("div", { class: "popup" }, [
-            createElement("span", { class: "countdown" }, [`the game will start in ${countdown}s...`])
+            createElement("span", { class: "countdown" }, [`The game will start in ${countdown}s...`])
         ]);
         render(popup, container);
     }
@@ -15,8 +20,13 @@ export default function renderPopup(_, container) {
     const interval = setInterval(() => {
         countdown--;
         renderCountdown();
+
         if (countdown <= 0) {
             clearInterval(interval);
+            container._popupInterval = null;
         }
     }, 1000);
+
+    // Save interval to container so we can clean it up next time
+    container._popupInterval = interval;
 }
