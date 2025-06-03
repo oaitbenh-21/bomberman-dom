e4275e6c7682c5737bcc96fc3234aa712aad5dcf
ielharra <ielharra@gmail.com>
2025-06-03 22:01:52 +0100

    fix

--- frontend/components/wiatingRoom.js
+++ frontend/components/wiatingRoom.js
@@ -1,16 +1,23 @@
 import { createElement, render, } from "../../mini-framework/src/mini-framework-z01.js";
 
 
+let timer = null;
+
 function renderWaitingRoom(gameState, container) {
-      // 🧹 Clear any existing interval
+      // Clear existing interval
       let currentInterval = gameState.getState().currentInterval;
       if (currentInterval) {
-            console.log('found interval in waiting room:', currentInterval)
+            console.log('found interval in waiting room:', currentInterval);
             clearInterval(currentInterval);
       }
 
-      let timer = gameState.getState().count;
 
+      if (!timer) {
+            timer = gameState.getState().count
+      }
+
+
+      // Renders countdown with latest timer value
       const renderCountdown = () => {
             const state = gameState.getState();
             const name = state.name.get();
@@ -19,7 +26,7 @@ function renderWaitingRoom(gameState, container) {
 
             const waiting = createElement("div", { class: "waitingRoom" }, [
                   createElement("h2", {}, `Welcome, ${name}`),
-                  createElement("p", {}, [`Waiting for other players ${timer}s...`]),
+                  createElement("p", {}, [`Waiting for other players ${timer}s...`]),  // use latest timer here
                   createElement('span', { class: "countDown" }, `players ${players.length}/4`),
                   createElement(
                         "ul",
@@ -35,23 +42,25 @@ function renderWaitingRoom(gameState, container) {
 
       renderCountdown(); // initial render
 
-      // ⏱️ Start new interval and store it on the container
-      const currentState = gameState.getState();
-      gameState.getState().currentInterval = setInterval(() => {
-            console.log('im still working ....................................................................')
-            const currentPlayers = Object.entries(currentState.players);
+      // Start new interval and store it
+      const newInterval = setInterval(() => {
+            const currentPlayers = Object.entries(gameState.getState().players);
 
             if (currentPlayers.length > 1) {
                   timer--;
                   renderCountdown();
 
                   if (timer <= 0) {
-                        clearInterval(currentInterval);
-                        currentInterval = null;
+                        clearInterval(newInterval);
+                        gameState.getState().currentInterval = null;
                   }
             }
       }, 1000);
+
+      // Save new interval ID in state
+      gameState.getState().currentInterval = newInterval;
 }
+
 
 
 export default renderWaitingRoom;
